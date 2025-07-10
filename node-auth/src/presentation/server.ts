d4a49b6bca1cde627a48777054544eb9e2d79ba7
Fernando Herrera <herrera.fernando1209@gmail.com>
2025-07-10 01:23:37 -0300

    add dto registerUser

--- node-auth/src/presentation/server.ts
+++ node-auth/src/presentation/server.ts
@@ -1,20 +1,31 @@
-import express from "express";
+import express, { Router } from "express";
 
 interface Options {
   port?: number;
+  routes: Router;
 }
 export class Server {
   public readonly app = express();
   private readonly port: number;
+  private readonly routes: Router;
 
   constructor(options: Options) {
-    const { port = 3000 } = options;
+    const { port = 3000, routes } = options;
     this.port = port;
+    this.routes = routes;
   }
 
   async start() {
-    this.app.listen(this.port);
+    //Middleware
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
 
-    console.log(`Server running on port ${this.port}`);
+    //Routes
+    this.app.use(this.routes);
+
+    //Escuchar el puerto
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
   }
 }
